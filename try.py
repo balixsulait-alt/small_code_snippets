7575654431695f4d1da47fad64eb34d028519d21
Bin Sula <balixsulait@gmail.com>
2026-04-14 16:01:40 +0300

    Update try.py

--- try.py
+++ try.py
@@ -10,7 +10,7 @@ except ZeroDivisionError: #error handling
 
 
 print(\n next \t error check \n)
-#######🤞🤞🤞 ######################################
+#######🤞🤞🤞 #####################
     # Attempt to open report.txt and handle the case where the file is missing
 try:
     report_file = open("report.txt", "r")
